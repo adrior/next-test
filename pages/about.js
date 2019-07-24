d44fcdf639e394ca4411e5a14797e3a854fb3166
adrior <adrior@ya.ru>
2019-07-24 07:06:02 +0300

    Fetch

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,24 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 
-export default function About() {
-  return (
-    <Layout>
-      <p>This is the about page</p>
-    </Layout>
-  );
-}
+import fetch from "isomorphic-unfetch";
+
+const About = props => (
+  <Layout>
+    <p>This is the about page</p>
+    <p>{props.messege}</p>
+  </Layout>
+);
+
+About.getInitialProps = async function() {
+  const res = await fetch("http://localhost:3000/api/photos/");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    messege: data.messege
+  };
+};
+
+export default About;
